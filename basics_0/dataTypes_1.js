e2777f13c55c55f8c82c77e50c9c10a63591998e
unknown <intekafq@gmail.com>
2023-12-03 09:41:32 +0300

    variable & datatypes

---
+++ basics_0/dataTypes_1.js
@@ -0,0 +1,17 @@
+// dataTypes in JS (2023)
+
+// number => 2 to 53 power 
+// bigint
+// string => ""
+// boolean => true or false
+// null
+
+const UserName = "intekhab" //string
+const UserAge = 27 //number 
+const isLoggedIn = false // boolean
+
+console.table([UserName, UserAge, isLoggedIn])
+
+console.log(typeof UserName)
+console.log(typeof UserAge)
+console.log(typeof isLoggedIn)
---
+++ basics_0/variables_0.js
@@ -0,0 +1,19 @@
+const accountId = 35597
+let accountEmail = "intekhab@google.com"
+var accountPassword = "21525252"
+accountCity = "sikar"
+let accountState
+
+/*
+ prefer not to use let
+ becuse of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+const userId = 35597;
+const userName = "Intekhab Qureshi"
+const userEmail = "intekafq@gmail.com"
+const userCity = "Losal"
+
+console.table([userId, userName, userEmail, userCity])
