d86b944faa76406b6b23e2cd668c71e41588081a
unknown <intekafq@gmail.com>
2023-12-06 15:40:02 +0300

    conversion opertations

---
+++ basics_0/conversionOperations_2.js
@@ -0,0 +1,38 @@
+// let score = false
+
+// console.log(typeof score)
+
+
+// check for type write (typeof)
+
+
+
+// let value = Number(score)
+
+// console.log(typeof value)
+// console.log(value)
+
+
+// "33" => output => 33
+// "33df" => output => NaN 
+// true => output => 1
+// false => output => 0
+
+let isLoggedIn = 0
+
+let isLoggedInBoolean = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn)
+// console.log(isLoggedInBoolean)
+
+
+
+let office = "open daily"
+
+let officeHR = Boolean(office)
+// console.log(officeHR)
+
+
+let randnumber = 55;
+
+let stringnumber = String(randnumber)
+console.log(typeof stringnumber)
